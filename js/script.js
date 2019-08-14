b19a6d73796ef943a6dd74962a9c090f4dacba95
ssrn <pe6i.is4o@gmail.com>
2019-08-14 17:12:40 +0300

    Modify catalog page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
 const mobileMenu = () => {
   const hamburger = document.querySelector('.main-nav__trigger');
-
   hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('is-active');
+  });
+};
+
+const catalogViewHandler = () => {
+  const catalogViews = document.querySelectorAll(".catalog-filters-views__item");
+  const productsWrap = document.querySelector(".products__wrap");
+  catalogViews.forEach(function (view) {
+    view.addEventListener("click", function () {
+      let inactiveCatalogView = document.querySelector(".catalog-filters-views__item:not(.is-active)");
+      if (this === inactiveCatalogView) {
+        document.querySelector(".catalog-filters-views__item.is-active").classList.remove("is-active");
+        this.classList.add("is-active");
+        productsWrap.classList.toggle("products__wrap--table");
+        productsWrap.classList.toggle("products__wrap--list");
+      }
+    })
+  })
+};
+
+const catalogFiltersHandler = () => {
+  const filterButtons = document.querySelectorAll(".catalog-sidebar__filter-button");
+  filterButtons.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      let inactiveFilterButton = document.querySelector(".catalog-sidebar__filter-button:not(.is-active)");
+      inactiveFilterButton.classList.add("is-active");
+      this.classList.remove("is-active");
+
+      if (document.querySelector(".catalog-sidebar__filter-button--hide.is-active")) {
+        document.querySelector(".catalog-nav").style.display = "block";
+      } else {
+        document.querySelector(".catalog-nav").style.display = "none";
+      }
+    })
   });
 };
 
@@ -26,22 +58,7 @@ const popupHandler = () => {
   });
 };
 
-const catalogViewHandler = () => {
-  const catalogViews = document.querySelectorAll(".catalog-filters-views__item");
-  const productsWrap = document.querySelector(".products__wrap");
-  catalogViews.forEach(function (view) {
-    view.addEventListener("click", function () {
-      let inactiveCatalogView = document.querySelector(".catalog-filters-views__item:not(.is-active)");
-      if (this === inactiveCatalogView) {
-        document.querySelector(".catalog-filters-views__item.is-active").classList.remove("is-active");
-        this.classList.add("is-active");
-        productsWrap.classList.toggle("products__wrap--table");
-        productsWrap.classList.toggle("products__wrap--list");
-      }
-    });
-  });
-};
-
 mobileMenu();
 catalogViewHandler();
+catalogFiltersHandler();
 popupHandler();
